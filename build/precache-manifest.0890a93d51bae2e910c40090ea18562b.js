61bc1f1f149b4e918973e3e77192b9a1447b1a67
Teerapat Jenrungrot <mjenrungrot@hmc.edu>
2018-12-20 13:28:45 -0800

    Test react (#6)
    * Test React
    
    * move folder
    
    * modify package.json

---
+++ build/precache-manifest.0890a93d51bae2e910c40090ea18562b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "ae4b9a361744de50f295",
+    "url": "/react-gh-pages/static/js/main.ae4b9a36.chunk.js"
+  },
+  {
+    "revision": "0a7a1053aaedd785abb5",
+    "url": "/react-gh-pages/static/js/1.0a7a1053.chunk.js"
+  },
+  {
+    "revision": "ae4b9a361744de50f295",
+    "url": "/react-gh-pages/static/css/main.aeecd4d9.chunk.css"
+  },
+  {
+    "revision": "7a88012bbdc0cc25aa73ad2ff899dfb3",
+    "url": "/react-gh-pages/index.html"
+  }
+];
