759c1ad60711df9d7737bcb063a2fa18b219b9a1
Teerapat Jenrungrot <mjenrungrot@hmc.edu>
2018-12-28 05:44:22 -0800

    initial build subtree commit

---
+++ mjenrungrot.github.io/build/precache-manifest.57bacbf8ac0b24546f598ecf8fe3932b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2abbbdecb0876a410481",
+    "url": "/static/css/main.59899734.chunk.css"
+  },
+  {
+    "revision": "2abbbdecb0876a410481",
+    "url": "/static/js/main.2abbbdec.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ada1ee4033783045abbd36bc95776e79",
+    "url": "/index.html"
+  }
+];
