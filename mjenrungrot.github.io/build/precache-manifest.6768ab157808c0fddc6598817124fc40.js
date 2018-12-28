edf20e6943ca7d4193b2150dfa7d13ca915c15fa
Teerapat Jenrungrot <mjenrungrot@hmc.edu>
2018-12-28 06:07:58 -0800

    test

---
+++ mjenrungrot.github.io/build/precache-manifest.6768ab157808c0fddc6598817124fc40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "11f9b6c170110687fc5e",
+    "url": "/static/css/main.94f5d8ab.chunk.css"
+  },
+  {
+    "revision": "11f9b6c170110687fc5e",
+    "url": "/static/js/main.11f9b6c1.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "ce31c1cd821aa1ebaa00ff34099bcb5a",
+    "url": "/index.html"
+  }
+];
