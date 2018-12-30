e1592e25269d9d2faeff4b3686889ef754602295
Teerapat Jenrungrot <mjenrungrot@hmc.edu>
2018-12-29 17:13:13 -0800

    Fix test

--- mjenrungrot.github.io/src/views/Index.test.js
+++ mjenrungrot.github.io/src/views/Index.test.js
@@ -1,11 +1,17 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import ReactDOM from "react-dom";
+import { MemoryRouter } from "react-router-dom";
 
 import Index from "./Index";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<Index />, div);
+  ReactDOM.render(
+    <MemoryRouter>
+      <Index />
+    </MemoryRouter>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
