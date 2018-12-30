1ff5cde684b8d38f55b626a352f1cc17d4c041a4
Teerapat Jenrungrot <mjenrungrot@hmc.edu>
2018-12-29 16:11:12 -0800

    re-test

--- mjenrungrot.github.io/src/views/Index.js
+++ mjenrungrot.github.io/src/views/Index.js
@@ -1,3 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+// eslint-disable-next-line no-unused-vars
+import ReactDOM from "react-dom";
+
 import Main from "../layouts/Main";
 
 const Index = () => <Main />;
--- mjenrungrot.github.io/src/views/Index.test.js
+++ mjenrungrot.github.io/src/views/Index.test.js
@@ -1,4 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
 import ReactDOM from "react-dom";
+
+import Index from "./Index";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
