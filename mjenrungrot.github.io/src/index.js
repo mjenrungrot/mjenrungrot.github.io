7f14b696d213ecb6ac716ac8a33ef51c3dcdf194
Teerapat Jenrungrot <mjenrungrot@hmc.edu>
2018-12-31 14:16:06 -0800

    Change AppBar color

--- mjenrungrot.github.io/src/index.js
+++ mjenrungrot.github.io/src/index.js
@@ -2,23 +2,42 @@ import React from "react"; //eslint-disable-line
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "typeface-roboto";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
 // Pages
 import Index from "./views/Index";
 
 import * as serviceWorker from "./serviceWorker";
 
-// CSS
-require("./static/css/main.scss");
+// Material-UI Theme
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#72cff8",
+      main: "#4fc3f7",
+      dark: "#3788ac"
+    },
+    secondary: {
+      light: "#53c4f7",
+      main: "#29b6f6",
+      dark: "#1c7fac"
+    }
+  },
+  typography: {
+    useNextVariants: true
+  }
+});
 
 document.title = "Personal Webpage -- Teerapat Jenrungrot";
 document.body.style.margin = "0";
 ReactDOM.render(
-  <Router>
-    <Switch>
-      <Route path="/" exact component={Index} />
-    </Switch>
-  </Router>,
+  <MuiThemeProvider theme={theme}>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Index} />
+      </Switch>
+    </Router>
+  </MuiThemeProvider>,
   document.getElementById("root")
 );
 
