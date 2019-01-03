2e32cada651b61915535285e2a797df8ecdacef9
Teerapat Jenrungrot <mjenrungrot@hmc.edu>
2019-01-02 21:00:52 -0800

    Add publications list

---
+++ mjenrungrot.github.io/src/data/publications.js
@@ -0,0 +1,36 @@
+// @flow
+
+const data = [
+  {
+    title: "aaa",
+    authors: ["aaaa bbbb", "cccc dddd"],
+    link: "ccc",
+    image: "",
+    date: "2015-11-20",
+    pdf: "",
+    code: "",
+    desc: "aaaaaaaaaaaa" + "dsfdfadfadasdasdsad " + " sdfdfsfkdsfjdslfjskdlfj. "
+  },
+  {
+    title: "aaa",
+    authors: ["aaaa bbbb", "cccc dddd"],
+    link: "ccc",
+    image: "",
+    date: "2015-11-20",
+    pdf: "",
+    code: "",
+    desc: "aaaaaaaaaaaa" + "dsfdfadfadasdasdsad " + " sdfdfsfkdsfjdslfjskdlfj. "
+  },
+  {
+    title: "aaa",
+    authors: ["aaaa bbbb", "cccc dddd"],
+    link: "ccc",
+    image: "",
+    date: "2015-11-20",
+    pdf: "",
+    code: "",
+    desc: "aaaaaaaaaaaa" + "dsfdfadfadasdasdsad " + " sdfdfsfkdsfjdslfjskdlfj. "
+  }
+];
+
+export default data;
