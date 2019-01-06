09c8d5365f98326fa1f3acef316723d5bff8a1b2
Teerapat Jenrungrot <mjenrungrot@hmc.edu>
2019-01-05 20:21:19 -0800

    Add eslint to blog.js

--- mjenrungrot.github.io/src/components/Blog.js
+++ mjenrungrot.github.io/src/components/Blog.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 // @flow
 
 import React from "react"; // eslint-disable-line no-unused-vars
@@ -13,11 +12,10 @@ import {
   CardContent,
   CardMedia,
   Hidden,
-  Button,
   Divider
 } from "@material-ui/core";
 
-import Markdown from './Template/Markdown';
+import Markdown from "./Template/Markdown";
 import post1 from "../data/blogs/blog-post.1.md";
 import post2 from "../data/blogs/blog-post.2.md";
 
@@ -29,7 +27,7 @@ const styles = theme => ({
   },
   mainFeaturedPost: {
     backgroundColor: theme.palette.grey[800],
-    color: theme.palette.white,
+    color: theme.palette.common.white,
     marginBottom: theme.spacing.unit * 4
   },
   mainFeaturePostContent: {
@@ -60,16 +58,18 @@ const styles = theme => ({
 
 const featuredPosts = [
   {
-    title: "Featured post",
-    date: "Nov 12",
+    title: "React Site",
+    date: "January 5",
     description:
-      "This is a wider card with supporting text below as a natural lead-in to additional content."
+      "I created my personal site using React. This site will be used to keep updates on my works and other random stuffs.",
+    link: null
   },
   {
     title: "Post title",
-    date: "Nov 11",
+    date: "January 5",
     description:
-      "This is a wider card with supporting text below as a natural lead-in to additional content."
+      "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    link: null
   }
 ];
 
@@ -91,11 +91,16 @@ function Blog(props) {
           <Paper className={classes.mainFeaturedPost}>
             <Grid container>
               <div className={classes.mainFeaturePostContent}>
-                <Typography component="h1" variant="h3" color="inherit" gutterBottom>
-                  Title of a longer featured blog post
+                <Typography
+                  component="h1"
+                  variant="h3"
+                  color="inherit"
+                  gutterBottom
+                >
+                  Blog
                 </Typography>
                 <Typography variant="h5" color="inherit" paragraph>
-                  Multiple lines of text
+                  Technical and random contents
                 </Typography>
               </div>
             </Grid>
@@ -108,19 +113,29 @@ function Blog(props) {
                 <Card className={classes.card}>
                   <div className={classes.cardDetails}>
                     <CardContent>
-                      <Typography component="h2" variant="h5">{post.title}</Typography>
-                      <Typography variant="subtitle1" color="textSecondary">{post.date}</Typography>
-                      <Typography variant="subtitle1" paragraph>{post.description}</Typography>
-                      <Typography variant="subtitle1" color="primary">Continue reading...</Typography>
+                      <Typography component="h2" variant="h5">
+                        {post.title}
+                      </Typography>
+                      <Typography variant="subtitle1" color="textSecondary">
+                        {post.date}
+                      </Typography>
+                      <Typography variant="subtitle1" paragraph>
+                        {post.description}
+                      </Typography>
+                      <Link to={post.link}>
+                        <Typography variant="subtitle1" color="primary">
+                          Continue reading...
+                        </Typography>
+                      </Link>
                     </CardContent>
-                    <Hidden xsDown>
-                      <CardMedia
-                        className={classes.cardMedia}
-                        image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
-                        title="Image title"
-                      />
-                    </Hidden>
                   </div>
+                  <Hidden xsDown>
+                    <CardMedia
+                      className={classes.cardMedia}
+                      image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
+                      title="Image title"
+                    />
+                  </Hidden>
                 </Card>
               </Grid>
             ))}
@@ -129,11 +144,14 @@ function Blog(props) {
           {/* Main content */}
           <Grid item>
             <Typography variant="h6" gutterBottom>
-              From the Firehose
+              Contents
             </Typography>
             <Divider />
             {posts.map(post => (
-              <Markdown className={classes.markdown} key={post.substring(0, 40)}>
+              <Markdown
+                className={classes.markdown}
+                key={post.substring(0, 40)}
+              >
                 {post}
               </Markdown>
             ))}
@@ -147,7 +165,6 @@ function Blog(props) {
 
 Blog.propTypes = {
   classes: PropTypes.object.isRequired
-}
+};
 
 export default withStyles(styles)(Blog);
-
