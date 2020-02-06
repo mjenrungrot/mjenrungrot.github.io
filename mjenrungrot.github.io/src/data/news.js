ad21503c6b729289f7295e89af5d3d1ac7117bea
Teerapat Jenrungrot <15001582+mjenrungrot@users.noreply.github.com>
2020-02-06 14:58:11 -0800

    add news

---
+++ mjenrungrot.github.io/src/data/news.js
@@ -0,0 +1,22 @@
+// @flow
+/* eslint-disable max-len */
+
+const data = [
+  {
+    date: 'February 2020',
+    caption: 'Journal paper `Using Cell Phone Pictures of Sheet Music To Retrieve MIDI Passages` accepted to IEEE Transactions on Multimedia',
+    link: null,
+  },
+  {
+    date: 'January 2020',
+    caption: 'Conference paper `A Board and Projects for an FPGA/Microcontroller-Based Embedded Systems Lab` submitted to GLSVLSI 2020',
+    link: null,
+  },
+  {
+    date: 'November 2019',
+    caption: 'Two conference papers `MIDI-Sheet Music Alignment Using Bootleg Score Synthesis` and `MIDI Passage Retrieval Using Cell Phone Pictures of Sheet Music` presented at ISMIR 2019, the Netherlands',
+    link: null,
+  },
+];
+
+export default data;
--- mjenrungrot.github.io/src/views/Home.js
+++ mjenrungrot.github.io/src/views/Home.js
@@ -10,14 +10,23 @@ import {withStyles} from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Fade from '@material-ui/core/Fade';
+import Link from '@material-ui/core/Link';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import AnnouncementIcon from '@material-ui/icons/Announcement';
 
 import Main from '../layouts/Main';
+import newsData from '../data/news';
+import './Home.css';
 
 const styles = (theme) => ({
   root: {
     ...theme.mixins.gutters(),
     paddingTop: theme.spacing(2),
     paddingBottom: theme.spacing(2),
+    marginBottom: theme.spacing(2),
   },
   paperFade: {
     in: true,
@@ -39,15 +48,39 @@ function Home(props) {
             About this site
           </Typography>
           <Typography component="p" gutterBottom>
-            Welcome to my personal website.
+            Welcome to my personal website. My name is Teerapat Jenrungrot.
           </Typography>
           <Typography component="p" gutterBottom>
-            Source available at{' '}
+            The source for this site is freely available at{' '}
             <a href="https://github.com/mjenrungrot/mjenrungrot.github.io">
               Here
             </a>
             .
           </Typography>
+        </Paper>
+      </Fade>
+      <Fade in>
+        <Paper className={classes.root}>
+          <Typography variant="h5" component="h3" gutterBottom>
+            News
+          </Typography>
+          <List dense="true">
+            {newsData.map((key) => {
+              return (
+                // eslint-disable-next-line react/jsx-key
+                <ListItem>
+                  <ListItemIcon>
+                    <AnnouncementIcon />
+                  </ListItemIcon>
+                  <ListItemText
+                    className={classes.newsText}
+                    primary={<Link to={key.link}>{key.caption}</Link>}
+                    primaryTypographyProps={{style: {color: 'red'}}}
+                    secondary={key.date} />
+                </ListItem>
+              );
+            })}
+          </List>
         </Paper>
       </Fade>
     </Main>
