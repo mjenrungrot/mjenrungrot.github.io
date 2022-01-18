d61f4073c846c57251df46cae33f48a49a1b0727
Teerapat Jenrungrot <15001582+mjenrungrot@users.noreply.github.com>
2022-01-18 01:26:09 -0800

    add equation + fix caption about

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,24 @@ import Layout from "@components/Layout";
 // core styles shared by all of react-notion-x (required)
 import "react-notion-x/src/styles.css";
 
-// used for collection views (optional)
+// used for collection views
 import "rc-dropdown/assets/index.css";
+
+// core styles for static tweet renderer
+import "react-static-tweets/styles.css";
+
+// used for equation
+import "katex/dist/katex.min.css";
+
+// used for code highlighting
+import "prismjs/themes/prism.css";
+import "prismjs/prism.js";
+import "prismjs/components/prism-go";
+import "prismjs/components/prism-markup";
+import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-typescript";
+import "prismjs/components/prism-graphql";
+import "prismjs/components/prism-bash";
 
 import "@styles/global.css";
 
